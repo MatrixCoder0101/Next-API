6b801df3eee9d88a30d17a57a4ae83251492eb7d
Goutam Kumar <tkgoutamm625@gmail.com>
2023-11-27 12:41:09 +0530

    update pages/api/anime.js

---
+++ pages/api/anime.js
@@ -0,0 +1,43 @@
+// Random Anime
+
+import fs from 'fs/promises';
+import path from 'path';
+
+const rootDirectory = process.cwd();
+const folders = ['Anime', 'Anime2'];
+
+async function getRandomImage(directory, directoryName) {
+  try {
+    const files = await fs.readdir(directory, { withFileTypes: true });
+
+    const imageFiles = files
+      .filter(file => file.isFile() && /\.(jpg|jpeg|png|gif)$/i.test(file.name))
+      .map(file => file.name);
+
+    if (imageFiles.length === 0) {
+      throw new Error(`No images found in ${directoryName}`);
+    }
+
+    const randomIndex = Math.floor(Math.random() * imageFiles.length);
+    const randomImage = imageFiles[randomIndex];
+    const imagePath = path.join(directory, randomImage);
+
+    return imagePath;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default async function handler(req, res) {
+  try {
+    const randomFolder = Math.random() < 0.5 ? 'Anime' : 'Anime2';
+    const randomDirectory = path.join(rootDirectory, 'public', randomFolder);
+
+    const imagePath = await getRandomImage(randomDirectory, randomFolder);
+
+    res.status(200).sendFile(imagePath);
+  } catch (error) {
+    console.error('Error serving random image:', error);
+    res.status(500).json({ error: `Internal Server Error: ${error.message}` });
+  }
+}
